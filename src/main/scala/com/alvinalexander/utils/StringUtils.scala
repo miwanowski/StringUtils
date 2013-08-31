6f475c675e5d6c23cbc0a6c8525a63876f6c0086
Alvin Alexander <devdaily@gmail.com>
2013-08-31 16:52:56 -0600

    adding more methods

--- src/main/scala/com/alvinalexander/utils/StringUtils.scala
+++ src/main/scala/com/alvinalexander/utils/StringUtils.scala
@@ -4,6 +4,40 @@ object StringUtils {
   
   def truncate(s: String, length: Int) = s.take(length)
 
+  /**
+   * Returns a String that is the same as the input String, but
+   * truncated to the specified length.
+   */
+  def truncateWithEllipsis(s: String, length: Int) = {
+    if (s.length > length) {
+      s.take(length) + "..."
+    } else {
+      s
+    }
+  }
+
+  /**
+   * A 'sanitize' method. Takes an input String, and returns a
+   * new String with all characters removed from that String
+   * other than letters and numbers.
+   */
+  def removeAllButLettersAndNumbers(s: String) = {
+    replaceAll(s, "[^a-zA-Z0-9]", "")
+  }
+  
+  /**
+   * @param s The string to perform the replace operation on (such as "123 Main Street")
+   * @param regex The regular expression to use to find what you want to replace.
+   * @param replaceWith The string you want to use as the replacement. Can be an empty string,
+   * asterisk, etc., anything you want to use as the replacement pattern.
+   */
+  def replaceAll(s: String, regex: String, replaceWith: String) = {
+    val r = regex.r
+    r.replaceAllIn(s, replaceWith)
+  }
+  
+  def lettersAndNumbersOnly_?(s: String) = s.matches("[a-zA-Z0-9]")
+
 }
 
 
--- src/test/scala/foo/StringUtilsTests.scala
+++ src/test/scala/foo/StringUtilsTests.scala
@@ -6,6 +6,14 @@ import com.alvinalexander.utils.StringUtils
 
 class StringUtilsTests extends FunSuite with BeforeAndAfter {
   
+  test("ellipsis tests") {
+    assert(StringUtils.truncateWithEllipsis("foo bar baz", 3) == "foo...")
+    assert(StringUtils.truncateWithEllipsis("foo bar baz", 7) == "foo bar...")
+    assert(StringUtils.truncateWithEllipsis("foo bar baz", 10) == "foo bar ba...")
+    assert(StringUtils.truncateWithEllipsis("foo bar baz", 20) == "foo bar baz")
+    assert(StringUtils.truncateWithEllipsis("foo bar baz", 11) == "foo bar baz")
+  }
+
   test("short strings") {
     assert(StringUtils.truncate("foo", 3) == "foo")
     assert(StringUtils.truncate("fooz", 3) == "foo")
@@ -19,6 +27,26 @@ class StringUtilsTests extends FunSuite with BeforeAndAfter {
     assert(StringUtils.truncate("foo bar baz", 3) == "foo")
     assert(StringUtils.truncate("foo bar baz", 4) == "foo ")
     assert(StringUtils.truncate("foo bar baz", 7) == "foo bar")
+  }
+  
+  test("remove all characters other than letters and numbers") {
+    assert(StringUtils.removeAllButLettersAndNumbers("foo bar baz") == "foobarbaz")
+    assert(StringUtils.removeAllButLettersAndNumbers("<script language=\"java\"") == "scriptlanguagejava")
+    assert(StringUtils.removeAllButLettersAndNumbers(" ") == "")
+  }
+
+  test("replace all characters other than letters and numbers") {
+    assert(StringUtils.replaceAll("foo bar baz", "[^a-zA-Z0-9]", "*") == "foo*bar*baz")
+    assert(StringUtils.replaceAll("<script language=\"java\"",  "[^a-zA-Z0-9]", "*") == "*script*language**java*")
+    assert(StringUtils.replaceAll("  ", "[^a-zA-Z0-9]", "*") == "**")
+  }
+
+  test("text that string has only letters and numbers") {
+    assert(StringUtils.lettersAndNumbersOnly_?("foo bar baz") == false)
+    assert(StringUtils.lettersAndNumbersOnly_?("<script") == false)
+    assert(StringUtils.lettersAndNumbersOnly_?("|") == false)
+    assert(StringUtils.lettersAndNumbersOnly_?("") == false)
+    assert(StringUtils.lettersAndNumbersOnly_?(" ") == false)
   }
 
 }
